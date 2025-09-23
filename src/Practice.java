9e1ce528d2b63c39770e819647c350a8ac73f795
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-09-23 12:45:31 -0700

    competed adults; tests passing

--- src/Practice.java
+++ src/Practice.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -64,7 +65,17 @@ public class Practice {
      * @throws NullPointerException if ages is null
      */
     public static Set<String> adults(Map<String, Integer> ages) {
-        return null;
+        if (ages == null) throw new NullPointerException();
+        
+        Set<String> adults = new HashSet<String>();
+
+        for (String person : ages.keySet()) {
+            if (ages.get(person) >= 18) {
+                adults.add(person);
+            }
+        }
+
+        return adults;
     }
 
     /**
